9cf333b95940b6bd396aeaae3b59901b53309635
darrenak403 <ngothanhdat4002@gmail.com>
2025-06-21 21:59:46 +0700

    feat: initialize frontend project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created Vite configuration file for React and Tailwind CSS integration.
    - Implemented basic App component with a button.
    - Developed reusable Button component with variant support.
    - Set up global styles using Tailwind CSS and custom CSS variables.
    - Added utility functions for class name management.
    - Configured TypeScript with appropriate settings for app and node environments.
    - Included SVG logo for Vite in the public directory.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold">Spotify Clone</h1>
+      <Button variant={"outline"}>this is a button</Button>
+    </>
+  )
+}
+
+export default App
