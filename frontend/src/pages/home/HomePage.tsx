f6c93a3bbb4cbf6118cc88c3dc590a784d0b996c
darrenak403 <ngothanhdat4002@gmail.com>
2025-06-22 22:07:07 +0700

    feat(auth): implement Zustand store for authentication management
    - Added `useAuthStore` to manage admin status, loading state, and error handling.
    - Implemented `checkAdminStatus` method to verify admin privileges via API call.
    - Included `reset` method to clear authentication state.

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,8 +1,26 @@
 import TopBar from "@/components/TopBar";
+import type {useMusicStore} from "@/stores/useMusicStore";
+import {useEffect} from "react";
 
 const HomePage = () => {
+  const {
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
+
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
   return (
-    <div>
+    <div className="rounded-md overflow-hidden">
       <TopBar />
     </div>
   );
