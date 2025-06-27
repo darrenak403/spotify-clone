f809e8b65efec38e69d8a94a48e182f63cdd7b7a
darrenak403 <ngothanhdat4002@gmail.com>
2025-06-27 12:46:17 +0700

    feat(home): enhance HomePage with FeaturedSection and SectionGrid components, add loading skeletons

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,6 +1,9 @@
 import TopBar from "@/components/TopBar";
-import type {useMusicStore} from "@/stores/useMusicStore";
+import {useMusicStore} from "@/stores/useMusicStore";
 import {useEffect} from "react";
+import FeaturedSection from "./components/FeaturedSection";
+import {ScrollArea} from "@/components/ui/scroll-area";
+import SectionGrid from "./components/SectionGrid";
 
 const HomePage = () => {
   const {
@@ -19,10 +22,33 @@ const HomePage = () => {
     fetchTrendingSongs();
   }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
 
+  console.log({isLoading, madeForYouSongs, featuredSongs, trendingSongs});
+
   return (
-    <div className="rounded-md overflow-hidden">
+    <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900 ">
       <TopBar />
-    </div>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good Afternoon
+          </h1>
+          <FeaturedSection />
+
+          <div className="space-y-8">
+            <SectionGrid
+              title="Made For You"
+              songs={madeForYouSongs}
+              isLoading={isLoading}
+            />
+            <SectionGrid
+              title="Trending"
+              songs={trendingSongs}
+              isLoading={isLoading}
+            />
+          </div>
+        </div>
+      </ScrollArea>
+    </main>
   );
 };
 
