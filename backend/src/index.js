f6eb3ede977b703c666e43cc64aaafa48fbd9cea
darrenak403 <ngothanhdat4002@gmail.com>
2025-09-03 22:35:27 +0700

    fix: remove commented-out code for static file serving in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -69,12 +69,6 @@ app.use("/api/songs", songRouter);
 app.use("/api/albums", albumRouter);
 app.use("/api/stats", statRouter);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist"))); // dùng để phục vụ các tệp tĩnh
